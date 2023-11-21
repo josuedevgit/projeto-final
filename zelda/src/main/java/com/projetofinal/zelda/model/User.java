ee6f223cb96eb77045b8fa4f611f424286635f48
Josué Silva <josueluzsilva.01@gmail.com>
2023-11-21 20:20:16 -0300

    in progress

---
+++ zelda/src/main/java/com/projetofinal/zelda/model/User.java
@@ -0,0 +1,20 @@
+package com.projetofinal.zelda.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String age;
+}
---
+++ zelda/src/main/java/com/projetofinal/zelda/service/UserService.java
@@ -0,0 +1,8 @@
+package com.projetofinal.zelda.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
