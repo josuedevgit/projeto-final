a0b5fc10dd5d7020d8dde4e519f3a339d988e5df
Josué Silva <josueluzsilva.01@gmail.com>
2023-11-21 16:13:51 -0300

    docs: configuração de ambiente

---
+++ zelda/src/main/java/com/projetofinal/zelda/ZeldaApplication.java
@@ -0,0 +1,13 @@
+package com.projetofinal.zelda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeldaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeldaApplication.class, args);
+	}
+
+}
