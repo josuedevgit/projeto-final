564380f50af5c4533b1053b856efca369051c495
biancaoliveirx <134893856+biancaoliveirx@users.noreply.github.com>
2023-11-23 15:59:30 -0300

    criando os pacotes

--- user-service/src/main/java/com/github/user_service/controller/UsuariosController.java
+++ user-service/src/main/java/com/github/user_service/controller/UsuariosController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/usuarios")
-public class UsuariosController {
+public class
+UsuariosController {
 
     @Autowired
     private UsuariosRepository usuarioRepository;
