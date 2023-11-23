35b0be6f465e288d0cdf0941441003d38be3450f
biancaoliveirx <134893856+biancaoliveirx@users.noreply.github.com>
2023-11-23 16:03:06 -0300

    criando a classe JogoZelda

---
+++ zelda-service/src/main/java/com/projetofinal/zeldaservice/model/JogoZelda.java
@@ -0,0 +1,9 @@
+package com.projetofinal.zeldaservice.model;
+
+public class JogoZelda {
+    private String nome;
+    private String descricao;
+    private String publicante;
+    private String dataLancamento;
+    private long id;
+}
