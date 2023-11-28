a3964cb16870d63ceb4887df5b767e9037d7e2bd
biancaoliveirx <134893856+biancaoliveirx@users.noreply.github.com>
2023-11-28 16:43:18 -0300

    excluindo pasta zelda (foi feita outra)

--- zelda/src/main/java/com/projetofinal/zelda/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.projetofinal.zelda.repository;
-
-import com.projetofinal.zelda.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
--- zelda/src/main/java/com/projetofinal/zelda/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.projetofinal.zelda.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserService {
-
-}
