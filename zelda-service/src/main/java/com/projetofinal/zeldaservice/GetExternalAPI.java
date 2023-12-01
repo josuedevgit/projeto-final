81044f4e8a6fe5659343919a2383625661d41829
biancaoliveirx <134893856+biancaoliveirx@users.noreply.github.com>
2023-12-01 14:55:01 -0300

    :feat: criada a requisição da API Zelda

--- zelda-service/src/main/java/com/projetofinal/zeldaservice/GetExternalAPI.java
+++ zelda-service/src/main/java/com/projetofinal/zeldaservice/GetExternalAPI.java
@@ -10,6 +10,17 @@ public class GetExternalAPI {
 	public static void main(String[] args) {
 		SpringApplication.run(GetExternalAPI.class, args);
 
+		String url ="https://zelda.fanapis.com/api/games";
+
+		WebClient.Builder builder = WebClient.builder();
+
+		String zelda = builder.build()
+				.get()
+				.uri(url)
+				.retrieve()
+				.bodyToMono(String.class)
+				.block();
+
 
 	}
 
