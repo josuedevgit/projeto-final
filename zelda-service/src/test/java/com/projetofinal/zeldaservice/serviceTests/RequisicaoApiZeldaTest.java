a04a95a16acbf413d890bfa95f88dc42c6d29937
Arthur <arthurmullercavalheiro@gmail.com>
2023-12-12 12:02:04 -0300

    :feat: Teste em desenvolvimento.

--- zelda-service/src/test/java/com/projetofinal/zeldaservice/serviceTests/RequisicaoApiZeldaTest.java
+++ zelda-service/src/test/java/com/projetofinal/zeldaservice/serviceTests/RequisicaoApiZeldaTest.java
@@ -1,10 +1,15 @@
 package com.projetofinal.zeldaservice.serviceTests;
 
 
+import com.projetofinal.zeldaservice.model.RetornoJogo;
 import com.projetofinal.zeldaservice.service.RequisicaoApiZeldaService;
+import com.projetofinal.zeldaservice.service.ZeldaService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import static org.mockito.Mockito.*;
 
 
 public class RequisicaoApiZeldaTest {
@@ -17,6 +22,21 @@ public class RequisicaoApiZeldaTest {
 
     @Test
     void testGetAll() {
+
+        //        WebClient webClient = mock(WebClient.class);
+
+        //          RetornoJogo retornoJogoEsperado = new RetornoJogo();
+        //          retornoJogoEsperado.setSuccess(true);
+
+        //       when(webClient.get()
+        //      .uri("https://zelda.fanapis.com/api/games")
+        //      .retrieve()
+        //           .bodyToMono(RetornoJogo.class))
+        //           .thenReturn(Mono.just(retornoJogoEsperado));
+
+        //      RetornoJogo resultado = zeldaService.getAll();
+
+        //   Assertions.assertEquals(retornoJogoEsperado, resultado);
 
     }
 }
